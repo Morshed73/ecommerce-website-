782453d5d54a796feeeff5218ced2f579294902a
Morshed73 <morshed73720@gmail.com>
2023-12-05 02:35:47 +0600

    header commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// header news
+$('.header-news-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    vertical: true,
+  });
