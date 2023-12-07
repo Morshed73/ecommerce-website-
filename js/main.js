1328d16f32e9d960f6ff01a1b39425850f3ddc86
Morshed73 <morshed73720@gmail.com>
2023-12-07 22:17:13 +0600

    Banner

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ $('.header-news-slider').slick({
     arrows: false,
     vertical: true,
   });
+  // location button
+  const locationButton = document.querySelector('.header-location-button');
+  locationButton.addEventListener('click', function(){
+    const locationDrop = document.querySelector('locationDropVisible')
+    locationDrop.style.display = "";
+    console.log("someone enterd");
+  })
+// banner slider
+$('.banner-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: '<i class="fa-solid fa-chevron-left prev-arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right next-arrow"></i>',
+});
