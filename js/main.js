dd28def5a4ce1dd143dc0a9ecc8a0070c090aa00
Morshed73 <morshed73720@gmail.com>
2024-01-31 20:46:56 +0600

    deals attached

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $('.catagories-slider').slick({
 $('.best-sells-slider').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 2000,
   arrows: true,
   prevArrow: '<i class="fa-solid fa-arrow-left prev-arrow"></i>',
@@ -49,6 +49,51 @@ $('.best-sells-slider').slick({
 let progress = document.querySelector('.progress');
 let sold = document.querySelector('.sold');
 sold.innerHTML = progress.value;
-progress.addEventListener("input", function(){
-  sold.innerHTML = this.value;
-})
+// Deals count down
+const days=document.querySelector('.days');
+const hours=document.querySelector('.hours');
+const minutes=document.querySelector('.minutes');
+const seconds=document.querySelector('.seconds');
+const OfferYear = new Date().getFullYear();
+const offerDate = new Date(`March 9 ${OfferYear} 00:00:00`)
+function countdownOfferTime(){
+  const presentTime = new Date();
+  const offerEnds = offerDate - presentTime;
+  const daysLeft = Math.floor(offerEnds / 1000 / 60 / 60 / 24);
+  days.innerHTML = daysLeft;
+  if(days.innerHTML < 0){
+    days.innerHTML = '00';
+  }else if(days.innerHTML < 10){
+    days.innerHTML = '0' + daysLeft;
+  }else{
+    daysLeft;
+  }
+  const hoursLeft = Math.floor(offerEnds / 1000 / 60 / 60) %24;
+  hours.innerHTML = hoursLeft;
+  if(hours.innerHTML < 0){
+    hours.innerHTML = '00';
+  }else if(hours.innerHTML < 10){
+    hours.innerHTML = '0' + hoursLeft;
+  }else{
+    hoursLeft;
+  }
+  const minutesLeft = Math.floor(offerEnds / 1000 / 60) %60;
+  minutes.innerHTML = minutesLeft;
+  if(minutes.innerHTML < 0){
+    minutes.innerHTML = '00';
+  }else if(minutes.innerHTML < 10){
+    minutes.innerHTML = '0' + minutesLeft;
+  }else{
+    minutesLeft;
+  }
+  const secondsLeft = Math.floor(offerEnds / 1000 / 60 / 60) %24;
+  seconds.innerHTML = secondsLeft;
+  if(seconds.innerHTML < 0){
+    seconds.innerHTML = '00';
+  }else if(hours.innerHTML < 10){
+    seconds.innerHTML = '0' + secondsLeft;
+  }else{
+    secondsLeft;
+  }
+}
+setInterval(countdownOfferTime);
